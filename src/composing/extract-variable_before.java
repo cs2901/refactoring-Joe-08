18ed57feef3b3108b351f5e089810484a64d5f46
Joe-08 <joe_andre0710@hotmail.com>
2019-06-03 09:49:11 -0500

    Agregando otro bool

--- src/composing/extract-variable_before.java
+++ src/composing/extract-variable_before.java
@@ -1,6 +1,7 @@
 void renderBanner() {
     final boolean platUpperMAC = platform.toUpperCase().indexOf("MAC") > -1;
     final boolean browUpperIE = browser.toUpperCase().indexOf("IE") > -1;
+    final boolean resized = resize > 0;
   if (platUpperMAC && browUpperIE && wasInitialized() && resize > 0 )
   {
     // do something

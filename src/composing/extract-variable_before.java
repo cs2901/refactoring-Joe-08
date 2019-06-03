d5a8e809282d1ba9b2085f873511f5509d16e6eb
Joe-08 <joe_andre0710@hotmail.com>
2019-06-03 09:45:33 -0500

    Dos ejercicios

--- src/composing/extract-variable_before.java
+++ src/composing/extract-variable_before.java
@@ -1,7 +1,7 @@
 void renderBanner() {
-    final boolean platUpper = platform.toUpperCase().indexOf("MAC") > -1;
-    final boolean browUpper = browser.toUpperCase().indexOf("IE") > -1;
-  if (platUpper && browUpper && wasInitialized() && resize > 0 )
+    final boolean platUpperMAC = platform.toUpperCase().indexOf("MAC") > -1;
+    final boolean browUpperIE = browser.toUpperCase().indexOf("IE") > -1;
+  if (platUpperMAC && browUpperIE && wasInitialized() && resize > 0 )
   {
     // do something
   }
